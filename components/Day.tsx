513d6f1cff3c85fda1fe2831959b4d41f8d9932f
Shashmit kumar <shaashmit@gmail.com>
2025-02-14 04:50:38 +0530

    weekday and dayjs setup

--- components/Day.tsx
+++ components/Day.tsx
@@ -1,18 +1,26 @@
-import { weekDays } from "@/utils/constants";
+import { localFormatter, weekDayFormatter, weekDays } from "@/utils/constants";
+import dayjs from "dayjs";
 import { View, Text } from "react-native";
 
-export function Day({ day }: { day: weekDays }) {
+export function Day({ day }: { day: string }) {
     return (
         <View 
             style={{
                 minHeight: `${100 / weekDays.length}%`,
-                paddingLeft: 12,
+                paddingLeft: 26,
                 padding: 4,
                 borderTopColor: "#dfdada",
                 borderTopWidth: 3,
             }}> 
-            <Text className='text-selected font-barlow-700 text-4xl'>{day}</Text>
-            <Text className='text-gray-500 font-barlow-400'>April, 14 2025 - 9:41</Text>
+            <Text className='text-selected font-barlow-700 text-4xl'>{dayjs(day).format(weekDayFormatter)}</Text>
+            <Text style={{
+                color: "#595757",
+                fontSize: 12,
+                fontFamily: "Barlow_400Regular",
+            }}>{dayjs(day).format(localFormatter)}</Text>
+            {/* <Todos
+                day={day}
+            /> */}
         </View>
     )
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,24 @@
+import dayjs from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek';
 
-export const weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ] as const;
+dayjs.extend(isoWeek);
+
+// export const weekDays = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ] as const;
   
   export type weekDays = typeof weekDays[number];
+
+  export const globalFormatter = 'YYYY-MM-DD'
+  export const localFormatter = `MMMM, DD YYYY - h:mma`
+  export const weekDayFormatter = 'dddd'
+
+  export const weekDays = [...Array(7).keys()].map((key)=>{
+    return dayjs().startOf('isoWeek').add(key, 'day').format(globalFormatter)
+  })
